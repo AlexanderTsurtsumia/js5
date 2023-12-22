5ae16ad8b60f131621826590203fe659efee9403
AlexanderTsurtsumia <127110773+AlexanderTsurtsumia@users.noreply.github.com>
2023-12-22 19:25:34 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const age=18;
+
+switch (age){
+    case 176:
+        window.close();
+    case 10:
+        window.alert("Error404");
+        break;
+    case 16:
+        document.write("<b>try again!</b>");
+        break;
+    default:
+        document.write("სწორია");
+}
